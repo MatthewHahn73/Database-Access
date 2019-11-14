b7ccfce8b37e799971b8caa45b67aead074f781f
MatthewHahn73 <matthewhahn73@gmail.com>
2019-11-14 17:49:23 -0600

    11/14/2019 - Join Added

--- PyDatabase.py
+++ PyDatabase.py
@@ -15,47 +15,74 @@ import mysql.connector
 #"Select" - Retrieves data from tables given data
 #***************************************************************
 
-db = mysql.connector.connect(host="localhost", #Connecting to a local database
+#***************************************************************
+#4 Different types of join
+#Inner Join - Shows all records that are common in both tables based on given clause
+#Left Join - Shows all records from the left linked table, even if no right table records exist
+#Right Join - Shows all records from the right linked table, even if no left table records exist
+#Full Join - Shows all records from both tables, regardless of existing data
+#***************************************************************
+
+#Connecting to a local database
+DB = mysql.connector.connect(host="localhost",
                                user="root",
-                               passwd="",
+                               passwd="pass",
                                database="database1")
 
-MC = db.cursor(buffered = True) #Creating Database object
+#Creating Database object
+MC = DB.cursor(buffered = True) 
 
-#This code create the customers table in database 1
-MC.execute("Create table customers (name VARCHAR(255), address VARCHAR(255))")
+#Creating a new table
+#MC.execute("create table customers (name VARCHAR(255), address VARCHAR(255))")
+#MC.execute("create table users (id INT, name VARCHAR(255), fav INT)")
+#MC.execute("create table products (id INT, name VARCHAR(255))")
 
-#This code adds some additional information into that table (A Row)
-SQLCode = "Insert Into customers (name, address) Values (%s, %s)"
+#Using executemany() method to insert data into given table
+SQLCode = "insert into customers (name, address) Values (%s, %s)"
 Values = [('John', 'Highway 21'),
           ('Joe', 'Pebble Beach Rd'),
           ('Karen', 'Forest Grove St')]
 MC.executemany(SQLCode, Values)
-db.commit()
+DB.commit()
 
-#Any legal SQL statement can be passed through the execute statement
+#Using execute() and fetchall() methods to get data from a database
 MC.execute("select name, address from customers")
 Result = MC.fetchall()
+
+#Using Fetchone method to get only the first row 
+MC.execute("select * from customers")
+Result = MC.fetchone()
+
+#Limiting results 
+MC.execute("select * from customers limit 2")
+Result = MC.fetchall()
+
+#Deleting data from a database 
+MC.execute("delete from customers where name = 'John'")
+DB.commit()
+
+#Using '%s' Operator to prevent SQL injection attacks
+SQLCommand = "select * from customers where name = %s"
+Values = ("Karen", )
+MC.execute(SQLCommand, Values)
+Result = MC.fetchone()
+
+#Updating a Table
+SQLCommand = "update customers set address = 'Summerhill Rd' where address = 'Pebble Beach Rd'"
+MC.execute(SQLCommand)
+DB.commit()
+
+#Joining 2 tables (Inner/Left/Right Join)
+SQLCommand = "select users.name as user, \
+                products.name as fav \
+                    from users \
+                        inner join products on users.fav = products.id"  #Can also use left/right join
+MC.execute(SQLCommand)
+Result = MC.fetchall() 
 for x in Result:
     print(x)
 
-#Parts of a result can also be returned using fetchone() instead
-MC.execute("select * from customers")
-Result = MC.fetchone()
-print(Result)
-
-#Deleting data from a database is also used by the execute() function
-MC.execute("DELETE FROM customers")
-db.commit()
-print(MC.rowcount, "record(s) deleted")
-
-#To prevent SQL injection, a common hacking method, it is a good idea to use
-#the '%' operator as a placeholder for data
-SQLCommand = "select * from customers where name = %s"
-Name = ("John", )
-MC.execute(SQLCommand, Name)
-Result = MC.fetchone()
-print(Result)
-
-#Since this table is no longer useful, time to drop it
+#Dropping all tables
 MC.execute("drop table customers")
+MC.execute("drop table products")
+MC.execute("drop table users")
